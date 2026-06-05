a9f53219b5cc57fb53b8320c8de892cbe6f1a579
Florenciaccrr <florenciacraggi@gmail.com>
2026-06-05 20:52:07 -0300

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <thread>
+#include <cstdlib>
+#include <ctime>
+
+#include "productor.h"
 
 using namespace std;
 
-int main() {
-    cout << "Probando..." << endl;
-    
+int main()
+{
+    srand(time(NULL));
+
+    thread t1(productor);
+    thread t2(productor);
+    thread t3(productor);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
     return 0;
 }
